7126d5f9e8e212cc3b5a5533919452d91c033bd9
Hunter Adams <hunteradams10@gmail.com>
2022-04-14 17:55:07 +1000

    Feature 2 -- User Input -- built, tested and implemented.

--- story.rb
+++ story.rb
@@ -6,7 +6,6 @@ require 'tty-font'
 require 'tty-prompt'
 
 font = TTY::Font.new(:standard)
-if_okay = TTY::Prompt.new
 
 name = 'stranger'
 favourite_thing = 'nothing'
@@ -40,3 +39,26 @@ puts "Just sit still and think really hard about #{favourite_thing}."
 
 puts ' '
 puts ' '
+
+puts "So, the thing is... your plane went down about a day ago, you've been unconscious for a while."
+puts "... you're the only survivor -- you're okay, though."
+puts "..."
+
+loop do
+    puts "... are you okay? (Y/N)"
+    if_okay = STDIN.gets.chomp.downcase
+if if_okay == "y"
+    puts " "
+    puts "I knew it! Thank goodness for that, because you have to get out of here."
+    break
+elsif if_okay == "n"
+    puts " "
+    puts "I'm sorry to hear that... but you don't have time to feel bad, I'm afraid."
+    puts "You have to get out of here."
+    break
+else 
+    puts " "
+    puts "Invalid input, type 'Y' or 'N'".red
+    puts " "
+end
+end
