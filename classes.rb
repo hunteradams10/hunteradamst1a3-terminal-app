c1e0027e061626305cb3767f18f684fd3bc44146
Hunter Adams <hunteradams10@gmail.com>
2022-04-15 12:55:25 +1000

    Testing CharTiming Module to determine text output speed

---
+++ classes.rb
@@ -0,0 +1,27 @@
+module CharTiming
+    def CharTiming.slow(string, timeout)   
+      string.each_char do |c| 
+        print(c)
+        sleep(timeout)
+      end
+    end
+  end
+
+
+
+module HitPoints
+
+    def CurrentHitpoints
+    end
+
+    
+    def HitPoints.take(diff)
+        hit_points = i
+        i - diff
+        return
+    end
+
+end
+  
+  
+
