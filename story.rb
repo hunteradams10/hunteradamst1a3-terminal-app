8dcce4335503725954b4b17b3505a2149b4395c4
Hunter Adams <hunteradams10@gmail.com>
2022-04-17 12:34:41 +1000

    Item integration in wolf encounter. Functional and no conflicts.

--- story.rb
+++ story.rb
@@ -29,76 +29,76 @@ pet = ARGV[4].capitalize if ARGV[4]
 pastel = Pastel.new
 puts pastel.red(font.write('Temple  Island'))
 
-puts ' '
+# puts ' '
 
-puts '#############################################################################################'
-puts ' '
-puts 'WELCOME'.cyan
-puts ' '
-puts '#############################################################################################'
+# puts '#############################################################################################'
+# puts ' '
+# puts 'WELCOME'.cyan
+# puts ' '
+# puts '#############################################################################################'
 
-sleep(2)
+# sleep(2)
 
-print TTY::Box.frame { 'CHAPTER ONE                                                                 ' }
+# print TTY::Box.frame { 'CHAPTER ONE                                                                 ' }
 
-puts ' '
-puts ' '
-CharTiming.slow("Hello, #{name}. You must be exhausted, but I can see that you're #{mood}. ", 0.08)
-puts ' '
-CharTiming.slow("Maybe just sit here for a while and take it in -- look, the weather is #{weather}, ", 0.08)
-CharTiming.slow("and you don't have to do anything right now. Remember #{pet}? What a lovely creature. ", 0.08)
-puts ' '
-CharTiming.slow("Don't get stressed. Just sit still and think really hard about #{favourite_thing}. ", 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow("Hello, #{name}. You must be exhausted, but I can see that you're #{mood}. ", 0.08)
+# puts ' '
+# CharTiming.slow("Maybe just sit here for a while and take it in -- look, the weather is #{weather}, ", 0.08)
+# CharTiming.slow("and you don't have to do anything right now. Remember #{pet}? What a lovely creature. ", 0.08)
+# puts ' '
+# CharTiming.slow("Don't get stressed. Just sit still and think really hard about #{favourite_thing}. ", 0.08)
 
-puts ' '
-puts ' '
-CharTiming.slow('...', 1)
-puts ' '
-puts ' '
-CharTiming.slow('So...', 1)
-CharTiming.slow("the thing is... your plane went down about a day ago, you've been unconscious for a while. ", 0.08)
-puts ' '
-puts ' '
-CharTiming.slow('...', 1)
-CharTiming.slow("you're the only survivor -- you're okay, though. ", 0.08)
-puts ' '
-puts ' '
-CharTiming.slow('...', 1)
+# puts ' '
+# puts ' '
+# CharTiming.slow('...', 1)
+# puts ' '
+# puts ' '
+# CharTiming.slow('So...', 1)
+# CharTiming.slow("the thing is... your plane went down about a day ago, you've been unconscious for a while. ", 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow('...', 1)
+# CharTiming.slow("you're the only survivor -- you're okay, though. ", 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow('...', 1)
 
-loop do
-  CharTiming.slow("are you okay? (Y/N) \n".light_blue, 0.08)
-  if_okay = $stdin.gets.chomp.downcase
-  puts ' '
-  case if_okay
-  when 'y'
-    CharTiming.slow("I knew it! Thank goodness for that, because you have to get out of here. \n", 0.08)
-    break
-  when 'n'
-    CharTiming.slow("I'm sorry to hear that... but you don't have time to feel bad, I'm afraid. ", 0.08)
-    CharTiming.slow("You have to get out of here. \n", 0.08)
-    break
-  else
-    puts "Invalid input, type 'Y' or 'N'".red
-    puts ' '
-  end
-end
+# loop do
+#   CharTiming.slow("are you okay? (Y/N) \n".light_blue, 0.08)
+#   if_okay = $stdin.gets.chomp.downcase
+#   puts ' '
+#   case if_okay
+#   when 'y'
+#     CharTiming.slow("I knew it! Thank goodness for that, because you have to get out of here. \n", 0.08)
+#     break
+#   when 'n'
+#     CharTiming.slow("I'm sorry to hear that... but you don't have time to feel bad, I'm afraid. ", 0.08)
+#     CharTiming.slow("You have to get out of here. \n", 0.08)
+#     break
+#   else
+#     puts "Invalid input, type 'Y' or 'N'".red
+#     puts ' '
+#   end
+# end
 
-puts ' '
+# puts ' '
 
-CharTiming.slow("... looks like it's about to rain. Better get moving. ", 0.08)
-puts ' '
-CharTiming.slow('Take a quick look around before you go; you might find something useful. ', 0.08)
-puts ' '
-puts ' '
-CharTiming.slow(
-  'You look around the sandy shoreline and discover a pineapple, a photo of your loved one, and a ring. '.italic, 0.08
-)
-puts ' '
-puts ' '
-sleep 2
-CharTiming.slow('Unfortunately, you can only take one item. Choose wisely.', 0.08)
-puts ' '
-puts ' '
+# CharTiming.slow("... looks like it's about to rain. Better get moving. ", 0.08)
+# puts ' '
+# CharTiming.slow('Take a quick look around before you go; you might find something useful. ', 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow(
+#   'You look around the sandy shoreline and discover a pineapple, a photo of your loved one, and a ring. '.italic, 0.08
+# )
+# puts ' '
+# puts ' '
+# sleep 2
+# CharTiming.slow('Unfortunately, you can only take one item. Choose wisely.', 0.08)
+# puts ' '
+# puts ' '
 
 choice = prompt.select('What will you take? Select with ↑/↓ arrow keys, then Enter') do |option|
   option.choice 'Pineapple'
@@ -123,26 +123,26 @@ end
 
 chosen_item = choice
 
-CharTiming.slow("You hug your #{chosen_item} to you, and make your way into the jungle ahead ".italic, 0.08)
-CharTiming.slow('and the rain begins to fall. '.italic, 0.08)
-CharTiming.slow('You run into the forest and a small stroke of fear creeps up your spine, '.italic, 0.08)
-CharTiming.slow('but you manage to take shelter under a tree, and the rain stops buffeting you.'.italic, 0.08)
-sleep 3
-puts ' '
-puts ' '
-CharTiming.slow("... there's a low growling nearby. Beside you, about a meter away, is a wolf. ".italic, 0.08)
-puts ' '
-puts ' '
-CharTiming.slow('... a presumably hungry wolf.'.italic, 0.08)
-puts ' '
-puts ' '
-CharTiming.slow("Okay, don't stress. You have full health. ".italic, 0.08)
-puts ' '
-puts ' '
-puts ' '
-puts "HP: #{hp.to_s.green.bold}"
-puts ' '
-puts ' '
+# CharTiming.slow("You hug your #{chosen_item} to you, and make your way into the jungle ahead ".italic, 0.08)
+# CharTiming.slow('and the rain begins to fall. '.italic, 0.08)
+# CharTiming.slow('You run into the forest and a small stroke of fear creeps up your spine, '.italic, 0.08)
+# CharTiming.slow('but you manage to take shelter under a tree, and the rain stops buffeting you.'.italic, 0.08)
+# sleep 3
+# puts ' '
+# puts ' '
+# CharTiming.slow("... there's a low growling nearby. Beside you, about a meter away, is a wolf. ".italic, 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow('... a presumably hungry wolf.'.italic, 0.08)
+# puts ' '
+# puts ' '
+# CharTiming.slow("Okay, don't stress. You have full health. ".italic, 0.08)
+# puts ' '
+# puts ' '
+# puts ' '
+# puts "HP: #{hp.to_s.green.bold}"
+# puts ' '
+# puts ' '
 
 choice = prompt.select('What will you do?') do |option|
   option.choice 'a) Reason with the wolf! (30% chance)'
@@ -158,7 +158,7 @@ when 'a) Reason with the wolf! (30% chance)'
     CharTiming.slow('You tell the wolf your story, and it is moved. It stops growling, ', 0.08)
     CharTiming.slow('and listens intently. ', 0.08)
     CharTiming.slow("'That's the saddest story I have ever heard.' ", 0.08)
-    CharTiming.slow('says the wolf.', 0.08)
+    CharTiming.slow('says the wolf. ', 0.08)
     CharTiming.slow('It can talk!!'.cyan, 0.08)
   elsif number > 30
     puts 'Unsuccessful'.light_red
@@ -207,3 +207,20 @@ CharTiming.slow("'You didn't need to freak out', says the wolf, 'I haven't ", 0.
 CharTiming.slow("seen anyone here for a long time. You can't be here on purpose. ", 0.08)
 CharTiming.slow("Lucky for you I'm not as mean as I look. I'll help you get out...", 0.08)
 CharTiming.slow("if you have a pineapple.'", 0.08)
+puts ' '
+puts ' '
+if chosen_item == "Pineapple"
+    CharTiming.slow("You show him your pineapple, and he takes it with glee. ".italic, 0.08)
+    CharTiming.slow("'Fantastic! These are my favourite.' ", 0.08)
+    CharTiming.slow("he looks around, ".italic, 0.08)
+    CharTiming.slow("'Hmm...' ", 0.08)
+    CharTiming.slow("He sniffs the air. ".italic, 0.08)
+else 
+    CharTiming.slow("You shake your head and tell him that you have only ".italic, 0.08)
+    CharTiming.slow("your #{chosen_item}. He clicks his tongue and shrugs. ".italic, 0.08)
+    CharTiming.slow("'What am I supposed to do with a #{chosen_item}? Oh well, ", 0.08)
+    CharTiming.slow("it was worth a shot. Don't worry, I'll still help you.' ", 0.08)
+    CharTiming.slow("He sniffs the air. ".italic, 0.08)
+end
+
+CharTiming.slow("'We'd better get going. I'll show you to where you need to go.' ", 0.08)
